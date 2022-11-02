036d0be4d47cd58e4cfde82daffcb49eec874f0a
Junhan0037 <wnsgks0037@naver.com>
2022-11-02 21:55:45 +0900

    fix: 게시판 만들기 - 초기화

--- project-board/src/main/java/com/board/repository/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.board.repository;
-
-import com.board.domain.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
-}
--- project-board/src/main/java/com/board/repository/querydsl/HashtagRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.board.repository.querydsl;
-
-import java.util.List;
-
-public interface HashtagRepositoryCustom {
-    List<String> findAllHashtagNames();
-}
